d2f814433634f42b977aed5374bd4abcc591d343
halg830 <halg830@gmail.com>
2023-10-03 17:14:39 -0500

    kevin

--- controllers/usuario.js
+++ controllers/usuario.js
@@ -1,9 +1,61 @@
 import bcryptjs from "bcryptjs"
 import { generarJWT } from "../middelwares/validar.js"
 import Usuario from "../models/usuario.js";
-import {validarJWT} from "../middelwares/validar.js";
 
 const httpusuario = {
+getusuario: async (req, res) => {
+    try {
+        const usuario = await Usuario.find()
+        res.json({usuario})
+    } catch (error) {
+        res.status(400).js({error})
+    }
+},
+getusuariocedula: async (req, res) =>{
+    const { cedula } = req.params
+    try {
+        const usuario = await Usuario.find({ cedula })
+        res.json({ usuario })
+    } catch (error) {
+        res.json({ error })
+    }
+},
+postAgregarusuario: async (req, res) => {
+    try {
+        const { nombre, cedula, telefono, usuario, password, rol } = req.body
+        const usuarios = new Usuario({nombre, cedula, telefono, usuario, password, rol})
+        const salt = bcryptjs.genSalt()
+        usuarios.password = bcryptjs.hashSync(password, salt)
+        await usuarios.save()
+        res.json({ usuarios })
+    } catch (error) {
+        res.status(400).json({ error })
+    }
+
+},
+putEditarusuario: async (req, res) => {
+    try {
+        const { id } = req.params
+        const {nombre, apellido, telefono, usuario , password} = req.body
+        const usuarios = await Usuario.findByIdAndUpdate(id,{nombre, apellido, telefono, usuario , password}, { new: true })
+        const salt = bcryptjs.genSaltSync();
+        usuarios.password = bcryptjs.hashSync(password, salt)
+        await usuarios.save()
+        res.json({ usuarios })
+    } catch (error) {
+        res.status(400).json({ error })
+    }
+},
+deleteusuario: async (req, res) => {
+    try {
+        const { id } = req.params
+        const usuarios = await Usuario.findByIdAndDelete(id)
+        res.json({usuarios})
+    } catch (error) {
+        res.status(400).json({ error })
+    }
+},
+
 login: async (req, res) => {
     const { usuario, password } = req.body;
 
