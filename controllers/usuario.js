c807cbe3f5e683c52d1048363f63ae48732edbaf
halg830 <halg830@gmail.com>
2023-10-03 13:17:49 -0500

    perras

---
+++ controllers/usuario.js
@@ -0,0 +1,39 @@
+
+
+login: async (req, res) => {
+    const { usuario, password } = req.body;
+
+    try {
+        const vendedor = await Vendedor.findOne({ usuario })
+        if (!vendedor) {
+            return res.status(400).json({
+                msg: "vendedor / Password no son correctos"
+            })
+        }
+
+        if (vendedor.estado === 0) {
+            return res.status(400).json({
+                msg: "vendedor Inactivo"
+            })
+        }
+
+        const validPassword = bcryptjs.compareSync(password, vendedor.password);
+        if (!validPassword) {
+            return res.status(401).json({
+                msg: "contraseña no son correctos"
+            })
+        }
+
+        const token = await generarJWT(vendedor.id);
+
+        res.json({
+            vendedor,
+            token
+        })
+
+    } catch (error) {
+        return res.status(500).json({
+            msg: "Hable con el WebMaster"
+        })
+    }
+}
