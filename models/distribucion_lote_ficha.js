14b7696dc294e5cdf7c8e175dee7732eb09c343c
EdwinDG <halg830@gmail.com>
2024-01-29 14:35:43 -0500

    si soy

---
+++ models/distribucion_lote_ficha.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const Distribucion_lote_ficha = new mongoose.Schema(
+    {
+        presupuesto: {type: String, require:true},
+        ficha: {type: String, require:true},
+        iddistribucion_presupuesto: {type:mongoose.Schema.Types.ObjectId, ref:'distribucion_presupuesto', require:true}
+    }
+)
+
+export default mongoose.model("Distribucion_lote_ficha", Distribucion_lote_ficha)
