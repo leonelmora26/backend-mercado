f1906370820c9aa79068ec48f17f7039b871c113
Nelson123ivn <santoscacuanelsonivan@gmail.com>
2024-01-31 17:12:11 -0500

    arreglos pedido3

--- controllers/detalle_pedido.js
+++ controllers/detalle_pedido.js
@@ -1,3 +1,27 @@
 import detalle_pedido from "../models/detalle_pedido";
 
-export default detalle_pedido
+const httpdetalle_pedido = {
+    // GET
+    getAlldetalle_pedido: async (req, res) => {
+        try {
+            const Detalle_pedido = await detalle_pedido.find()
+                .populate("idpedido", "fechacreacion")
+                .populate("idproductor", "nombre drescipcion precioUnitario");
+            res.json({ Detalle_pedido });
+        } catch (error) {
+            res.status(400).json({ error });
+        }
+    },
+
+    getdetalle_pedidoid: async (req, res) => {
+        const { id } = req.params
+        try {
+            const detalle_pedido = await Detalle_pedido.find({ id })
+            res.json({ item })
+        } catch (error) {
+            res.json({ error })
+        }
+    },
+
+};
+export default httpdetalle_pedido
