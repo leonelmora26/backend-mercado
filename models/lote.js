c1b5b1243b70cdf325be8c5a38ae13c265ecde78
Raul Yesid Rojas Ballesteros <131273447+YesidRojas06@users.noreply.github.com>
2024-02-15 14:10:39 -0500

    siek

--- models/lote.js
+++ models/lote.js
@@ -8,10 +8,5 @@ const lote = new Schema({
   estado:{type: Boolean, default:1},
 });
 
-lote.path('presupuesto').validate(function (value) {
-    // Utiliza una expresión regular para verificar si el valor es un número válido
-    const validNumberRegex = /^\d+(\.\d{1,2})?$/;
-    return validNumberRegex.test(value);
-  }, 'El presupuesto debe ser un número válido.');
 
 export default mongoose.model("lote", lote);
