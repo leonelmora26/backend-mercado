c13b837b44bd4ec693988f52d0528fb0bb626366
EdwinDG <halg830@gmail.com>
2024-02-21 13:35:39 -0500

    nuevo80

--- routes/usuario.js
+++ routes/usuario.js
@@ -11,25 +11,25 @@ router.get('/usuario/:cedula',[
     validarCampos
 ], httpusuario.getusuariocedula)
 router.post('/agregar',[
-    // check("nombre", "el nombre es obligatorio").not().isEmpty(),
-    // check("cedula", "la cedula es obligatoria").not().isEmpty(),
-    // check("cedula", "la cedula es obligatoria").custom(helpersUsuario.checkExistingUsuarioCode),
-    // check("telefono", "el telefono es obligatorio").not().isEmpty(),
-    // check("usuario", "el usuario es obligatorio").not().isEmpty(),
-    // check("usuario", "el usuario es obligatorio").custom(helpersUsuario.ExistingUsuario),
-    // check("password", "la contraseña es obligatoria").not().isEmpty(),
-    // check("rol", "el rol es obligatorio").not().isEmpty(),
+    check("nombre", "el nombre es obligatorio").not().isEmpty(),
+    check("cedula", "la cedula es obligatoria").not().isEmpty(),
+    check("cedula", "la cedula es obligatoria").custom(helpersUsuario.checkExistingUsuarioCode),
+    check("telefono", "el telefono es obligatorio").not().isEmpty(),
+    check("usuario", "el usuario es obligatorio").not().isEmpty(),
+    check("usuario", "el usuario es obligatorio").custom(helpersUsuario.ExistingUsuario),
+    check("password", "la contraseña es obligatoria").not().isEmpty(),
+    check("rol", "el rol es obligatorio").not().isEmpty(),
     validarCampos
 ],httpusuario.registroUsuario );
 router.put('/usuario/:id',[
-    // check("nombre", "el nombre es obligatorio").not().isEmpty(),
-    // check("cedula", "la cedula es obligatoria").not().isEmpty(),
-    // check("cedula", "la cedula es obligatoria").custom(helpersUsuario.checkExistingUsuarioCode),
-    // check("telefono", "el telefono es obligatorio").not().isEmpty(),
-    // check("usuario", "el usuario es obligatorio").not().isEmpty(),
-    // check("usuario", "el usuario es obligatorio").custom(helpersUsuario.ExistingUsuario),
-    // check("password", "la contraseña es obligatoria").not().isEmpty(),
-    // check("rol", "el rol es obligatorio").not().isEmpty(),
+    check("nombre", "el nombre es obligatorio").not().isEmpty(),
+    check("cedula", "la cedula es obligatoria").not().isEmpty(),
+    check("cedula", "la cedula es obligatoria").custom(helpersUsuario.checkExistingUsuarioCode),
+    check("telefono", "el telefono es obligatorio").not().isEmpty(),
+    check("usuario", "el usuario es obligatorio").not().isEmpty(),
+    check("usuario", "el usuario es obligatorio").custom(helpersUsuario.ExistingUsuario),
+    check("password", "la contraseña es obligatoria").not().isEmpty(),
+    check("rol", "el rol es obligatorio").not().isEmpty(),
     validarCampos], httpusuario.putEditarusuario);
 router.put('/inactivar/:id', httpusuario.putusuarioInactivar)
 router.put('/activar/:id', httpusuario.putusuarioActivar)
