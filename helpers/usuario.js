fedf5755362dfd3b09d319672d8808ae30ce7940
EdwinDG <halg830@gmail.com>
2024-02-21 13:34:42 -0500

    kfsdfjsd

--- helpers/usuario.js
+++ helpers/usuario.js
@@ -1,36 +1,30 @@
-import Usuario from "../models//usuario.js";
+import Usuario from "../models/usuario.js";
 
-const helpersUsuario ={
-    checkExistingUsuarioCode: async (cedula, req) => {
-         const existingUsuario = await Usuario.findOne({ cedula });
+const helpersUsuario = {
+  checkExistingUsuarioCode: async (cedula, req) => {
+    const existingUsuario = await Usuario.findOne({ cedula });
 
-         const {_id } = req.req.body
-         console.log(req.req.body);
-         if (existingUsuario) {
-            if (existingUsuario._id != _id && req.req.method === "PUT")
-              throw new Error("la cedula ya esta registrada en la base de datos.");
-            else if (req.req.method === "POST")
-              throw new Error("la cedula ya esta registrada en la base de datos.");
-          }
-      
-    
-      },
-      ExistingUsuario: async (usuario, req) => {
-        const existUsuario = await Usuario.findOne({ usuario });
+    const { _id } = req.req.body;
+    console.log(req.req.body);
+    if (existingUsuario) {
+      if (existingUsuario._id != _id && req.req.method === "PUT")
+        throw new Error("la cedula ya esta registrada en la base de datos.");
+      else if (req.req.method === "POST")
+        throw new Error("la cedula ya esta registrada en la base de datos.");
+    }
+  },
+  ExistingUsuario: async (usuario, req) => {
+    const existUsuarioo = await Usuario.findOne({ usuario });
 
-        const {_id } = req.req.body
-        console.log(req.req.body);
-        if (existUsuario) {
-           if (existUsuario._id != _id && req.req.method === "PUT")
-             throw new Error("El usuario ya esta registrada en la base de datos.");
-           else if (req.req.method === "POST")
-             throw new Error("El usuario ya esta registrada en la base de datos.");
-         }
-     
-   
-     },
+    const {_id } = req.req.body
+    console.log(req.req.body);
+    if (existUsuarioo) {
+       if (existUsuarioo._id != _id && req.req.method === "PUT")
+         throw new Error("El usuario ya esta registrada en la base de datos.");
+       else if (req.req.method === "POST")
+         throw new Error("El usuario ya esta registrada en la base de datos.");
+     }
+  }
+};
 
-}
-
-
-export default helpersUsuario
+export default helpersUsuario;
--- routes/lote.js
+++ routes/lote.js
@@ -9,9 +9,6 @@ const router = new Router();
 router.get("/all", httpLote.getAllLote);
 
 router.get("/buscar/:nombre", httpLote.getLoteNombre);
-
-router.get("/Lote/:presupuesto", httpLote.getLotePresupuesto);
-
 router.get("/Lote/:id", httpLote.getLoteId);
 
 router.post(
@@ -19,9 +16,6 @@ router.post(
   [
     check("nombre", "El nombre de lote es obligatorio").notEmpty(),
     check("nombre", "El nombre debe tener minimo 8 letras").isLength({ min: 4}), 
-    check("presupuesto", "Debe tener su presupuesto asignado").notEmpty(),
-    check("presupuesto", "Ingresa un presupuesto a asignar").isNumeric().withMessage("El presupuesto debe ser un número."),
-
     validarCampos 
   ],
   httpLote.postLote 
@@ -30,16 +24,10 @@ router.post(
 router.put("/editar/:id", [
   check("nombre", "Deseas cambiar el nombre").notEmpty(),
   check("nombre", "El nombre debe tener minimo 8 letras").isLength({ min: 8}), 
-  check("presupuesto", "Deseas cambiar el presupuesto actual").isNumeric().withMessage("El presupuesto debe ser un número."),
-
 validarCampos 
 ], httpLote.putLote); 
-
 router.put("/inactivar/:id", httpLote.putLoteInactivar); 
-
 router.put("/activar/:id", httpLote.putLoteActivar); 
-
-router.delete("/eliminar/:nombre", httpLote.deleteLote); 
 
 export default router;
 
