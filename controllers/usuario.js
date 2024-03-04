59de1f36862f81e4a91ed3ba8420b5c3be03e554
AngelB10 <131167121+AngelB10@users.noreply.github.com>
2024-03-04 15:54:11 -0500

    A

--- controllers/usuario.js
+++ controllers/usuario.js
@@ -1,7 +1,20 @@
 import bcryptjs from "bcryptjs"
 import { generarJWT } from "../middelwares/validar.js"
 import Usuario from "../models/usuario.js";
+import nodemailer from "nodemailer"
 
+
+let codigoEnviado = {};
+
+function generarNumeroAleatorio() {
+  let numeroAleatorio = Math.floor(Math.random() * 1000000);
+  let numero = numeroAleatorio.toString().padStart(6, "0");
+  let fechaCreacion = new Date();
+
+  codigoEnviado = { codigo: numero, fechaCreacion };
+
+  return numero;
+}
 const httpusuario = {
 getusuario: async (req, res) => {
     try {
@@ -131,7 +144,140 @@ login: async (req, res) => {
             msg: "Hable con el WebMaster"
         })
     }
-}
+},
+
+
+recuperarPassword: async (req, res) => {
+    try {
+      const { Correo } = req.body;
+
+      const usuario = await usuarios.findOne({ Correo });
+
+      if (!usuario)
+        return res.status(404).json({ error: "Usuario no encontrado" });
+
+      const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+          user: process.env.userEmail,
+          pass: process.env.password,
+        },
+      });
+   
+
+      
+      const  codigo = generarNumeroAleatorio()
+      const mailOptions = {
+        from: process.env.userEmail,
+        to: Correo,
+        subject: "Recuperación de Contraseña",
+        text:
+          "Su codigo es este: " + codigo, 
+      };
+
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          console.error(error);
+          res.status(500).json({
+            success: false,
+            msg: "Error al enviar el Correo electrónico.",
+          });
+        } else {
+          console.log("Correo electrónico enviado: " + info.response);
+          res.json({
+            success: true,
+            msg: "Correo electrónico enviado con éxito.",
+          });
+        }
+      });
+    } catch (error) {
+        console.log(error);
+      res.status(500).json({ error });
+    }
+  },
+
+  confirmarCodigo: async (req, res) => {
+    try {
+      const { codigo } = req.params;
+
+      if (!codigoEnviado) {
+        return res.status(400).json({ error: "Código no generado" });
+      }
+
+      const { codigo: codigoGuardado, fechaCreacion } = codigoEnviado;
+      const tiempoExpiracion = 30; // Tiempo de expiración en minutos
+
+      const tiempoActual = new Date();
+      const tiempoDiferencia = tiempoActual - new Date(fechaCreacion);
+      const minutosDiferencia = tiempoDiferencia / (1000 * 60);
+
+      if (minutosDiferencia > tiempoExpiracion) {
+        return res.status(400).json({ error: "El código ha expirado" });
+      }
+
+      if (codigo === codigoGuardado) {
+        return res.json({ msg: "Código correcto" });
+      }
+
+      return res.status(400).json({ error: "Código incorrecto" });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({
+        error: "Error, hable con el WebMaster",
+      });
+    }
+  },
+
+
+
+nuevaPassword: async (req, res) => {
+    try {
+      const { correo, codigo, password } = req.body;
+
+      const { codigo: codigoGuardado, fechaCreacion } = codigoEnviado;
+      const tiempoExpiracion = 30; // Tiempo de expiración en minutos
+
+      const tiempoActual = new Date();
+      const tiempoDiferencia = tiempoActual - new Date(fechaCreacion);
+      const minutosDiferencia = tiempoDiferencia / (1000 * 60);
+
+      if (minutosDiferencia > tiempoExpiracion) {
+        return res.status(400).json({ error: "El código ha expirado" });
+      }
+
+      if (codigo === codigoGuardado) {
+        codigoEnviado = {};
+
+        const usuario = usuarios.findOne({correo});
+
+        const salt = bcryptjs.genSaltSync();
+        const newPassword = bcryptjs.hashSync(password, salt);
+
+        await usuarios.findByIdAndUpdate(
+          usuario.id,
+          { password: newPassword },
+          { new: true }
+        );
+
+        return res
+          .status(200)
+          .json({ msg: "Contraseña actualizada con éxito" });
+      }
+
+      return res.status(400).json({ error: "Código incorrecto" });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({
+        error: "Error, hable con el WebMaster",
+      });
+    }
+  }
+
+
+
+
+
+
 }
 
 export default httpusuario
